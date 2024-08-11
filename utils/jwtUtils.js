1e9ecee7cb0aa01470f2a8e3bcbb40f505f4b9f4
abdoghazy2015 <64314534+abdoghazy2015@users.noreply.github.com>
2024-08-11 21:52:48 +0300

    Update jwtUtils.js

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
-import {randomBytes} from "crypto";
+import crypto from "crypto";
 
-const jwtSecret = randomBytes(32).toString('hex');
+const jwtSecret = crypto.randomBytes(32).toString('hex');
 
 export const generateToken = (userId, email) => {
   return jwt.sign({ userId, email }, jwtSecret, {
